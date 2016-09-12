43a478058367068b275d43360a07f9b0bf3c1128
natac13 <sean.campbell13@gmail.com>
2016-09-12 06:43:43 -0400

    forgot link text

--- app/components/Main/Main.jsx
+++ app/components/Main/Main.jsx
@@ -13,7 +13,7 @@ function Main(props) {
       <Header appName={appName} />
       Future upgrade coming soon...
       While your here fell free to check out my conduit fill application.
-      <a href="http://seancampbellnatac.com/conduit-fill/" />
+      <a href="http://seancampbellnatac.com/conduit-fill/">Here</a>
       <Footer appName={appName} />
     </div>
   );
