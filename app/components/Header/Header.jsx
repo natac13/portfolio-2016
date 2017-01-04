61cab91f2bb2b098f8daf667391ed4eab33fbd80
natac13 <sean.campbell13@gmail.com>
2017-01-04 09:30:53 -0500

    Have started to redo the portfolio page
    I have upgraded from
    React 15.0.1
    React-Dom 15.0.1
    React-addons-css-transition-group 15.3.1
    React-toolbox 0.16.2
    
    I removed react-toolbox-loader as the perpending of theme.scss file is now done with sassLoader under the data option.
    
    I started the header. Got the background image in and started on the social links.

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,16 +1,20 @@
 import React, { PropTypes } from 'react';
-
+import img from './main_background.jpg';
 import style from './style.scss';
+
+import SocialLinks from '../SocialLinks/';
 
 function Header(props) {
   const { appName, pathname } = props;
 
   return (
     <header className={style.wrapper}>
+      <img className={style.background} src={img} />
       {!!pathname ?
         <h1 className={style.title}>Contact Sean Campbell</h1> :
         <h1 className={style.title}>Sean Campbell's {appName}</h1>
       }
+      <SocialLinks />
     </header>
   );
 }
---
+++ app/components/SocialLinks/SocialLinks.jsx
@@ -0,0 +1,55 @@
+import React, { PropTypes } from 'react';
+import style from './style.scss';
+
+import { Button } from 'react-toolbox/lib/button';
+// import { Button, IconButton } from 'react-toolbox/lib/button';
+import tooltip from 'react-toolbox/lib/tooltip';
+import { Icon } from 'react-fa';
+
+const TooltipButton = tooltip(Button);
+// const TooltipIconButton = tooltip(IconButton);
+const TOOLTIP_DELAY = 550;
+
+function SocialLinks(props) {
+
+  return (
+    <section className={style.nav}>
+      <TooltipButton
+        className={style.socialLink}
+        label="LinkedIn"
+        flat
+        href="https://ca.linkedin.com/in/seancampbellnatac"
+        icon={<Icon name="linkedin" />}
+        neutral={false}
+        tooltip="LinkedIn Account"
+        tooltipDelay={TOOLTIP_DELAY}
+      />
+      <TooltipButton
+        className={style.socialLink}
+        label="Github"
+        flat
+        href="http://github.com/natac13"
+        icon={<Icon name="github" />}
+        neutral={false}
+        tooltip="Github Account"
+        tooltipDelay={TOOLTIP_DELAY}
+      />
+      <TooltipButton
+        className={style.socialLink}
+        label="Twitter"
+        flat
+        href="https://twitter.com/natac1311"
+        icon={<Icon name="twitter" />}
+        neutral={false}
+        tooltip="Twitter Account"
+        tooltipDelay={TOOLTIP_DELAY}
+      />
+    </section>
+  );
+}
+
+SocialLinks.propTypes = {
+
+};
+
+export default SocialLinks;
