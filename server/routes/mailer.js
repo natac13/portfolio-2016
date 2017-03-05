d8dde14950430b6209433f7cf5ddee8a7744ba65
natac13 <sean.campbell13@gmail.com>
2017-03-05 07:35:47 -0500

    try mailgun after email video

--- server/routes/mailer.js
+++ server/routes/mailer.js
@@ -2,14 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
-// const apiKey = process.env.MAILGUN_API_KEY;
+const apiKey = process.env.MAILGUN_API_KEY;
 const apiKey = process.env.SENDINBLUE_API_KEY;
 const domain = process.env.MAILGUN_DOMAIN;
 const toEmail = process.env.EMAIL;
 
-// const mailgun = require('mailgun-js')({ apiKey, domain });
-const sendinblue = require('sendinblue-api');
-const blueMail = new sendinblue({ apiKey })
+const mailgun = require('mailgun-js')({ apiKey, domain });
+// const sendinblue = require('sendinblue-api');
+// const blueMail = new sendinblue({ apiKey })
 
 function userResponse(success, subject, error) {
   if (!success) {
@@ -31,25 +31,31 @@ function userResponse(success, subject, error) {
 router.route('/')
   .post(async function(req, res, next) {
     const { subject, comments, userEmail, name } = req.body;
+    // const emailOptions = {
+    //   from: [ userEmail ],
+    //   to: { [toEmail]: 'Sean Campbell' },
+    //   subject: 'Website Contact',
+    //   text: comments,
+    // };
     const emailOptions = {
-      from: [ userEmail ],
-      to: { [toEmail]: 'Sean Campbell' },
+      from: userEmail,
+      to: toEmail,
       subject: 'Website Contact',
       text: comments,
     };
     // return res.json(userResponse(true, undefined));
 
-    blueMail.send_email(emailOptions, (err, response) => {
-      if (err) { return next(userResponse(false, '', err)); }
-      return res.json(userResponse(true, undefined));
-    });
-
-    // mailgun.messages().send(emailOptions, (err, body) => {
-    //   if (err) {
-    //     return next(userResponse(false, '', err));
-    //   }
+    // blueMail.send_email(emailOptions, (err, response) => {
+    //   if (err) { return next(userResponse(false, '', err)); }
     //   return res.json(userResponse(true, undefined));
     // });
+
+    mailgun.messages().send(emailOptions, (err, body) => {
+      if (err) {
+        return next(userResponse(false, '', err));
+      }
+      return res.json(userResponse(true, undefined));
+    });
   });
 
 export default router;
